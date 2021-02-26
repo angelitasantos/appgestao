276ffbd4156bfd864e440a9a460d7d53d8bf6126
Angelita Santos <angelitasantos@gmail.com>
2021-02-26 20:20:28 -0300

    created entrar panel file

--- painel/entrar.php
+++ painel/entrar.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=7,edge,chrome=1">
     <title>Entrar Painel de Controle</title>
-    <link rel="shortcut icon" href="<?php echo INCLUDE_PATH; ?>images/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo INCLUDE_PATH; ?>assets/images/logo.png" type="image/x-icon">
 
     <!-- Font Awesome, Google Font, CSS -->
     <script src="https://kit.fontawesome.com/f7cb610b49.js" crossorigin="anonymous"></script>
