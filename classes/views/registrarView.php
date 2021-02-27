4f0b390813c5b961e951f741eaf55633cf835005
Angelita Santos <angelitasantos@gmail.com>
2021-02-26 21:59:14 -0300

    created registrar view file

---
+++ classes/views/registrarView.php
@@ -0,0 +1,23 @@
+<?php
+
+    namespace views;
+
+    class registrarView {
+
+        public static $params = [];
+
+        public static function setParams($params) {
+            self::$params = $params;
+        }
+
+        public static function render($fileName,$header = 'pages/includes/header.php',$footer = 'pages/includes/footer.php') {
+            include($header);
+            include('pages/'.$fileName);
+            include($footer);
+            die();
+            
+        }
+
+    }
+
+?>
