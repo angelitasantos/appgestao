ca47c1e68576482ab9434fc1f71e6faca520e4ca
Angelita Santos <angelitasantos@gmail.com>
2021-02-26 21:08:52 -0300

    changed index panel file with class painel

--- painel/index.php
+++ painel/index.php
@@ -1,5 +1,14 @@
 <?php
     ob_start();
     include('../config.php');
+
+    if(Painel::login() == false){
+        include('entrar.php');
+    }else if($_SESSION['site'] == '1'){
+        include('main.php');
+    }else{
+        include('main.php');
+    }
     ob_end_flush();
+
 ?>
